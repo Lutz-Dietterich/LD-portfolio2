db0d7a7fbb41e4e24f89abec30295ae8c8791a38
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-05-30 14:01:03 +0200

    add Teaser

--- components/About/index.js
+++ components/About/index.js
@@ -1,11 +1,26 @@
+import styled from "styled-components";
+
 import Teaser from "../About/Teaser";
 import Skills from "../About/Skills";
 
 export default function About() {
   return (
-    <>
+    <StyledSection>
       <Teaser />
       <Skills />
-    </>
+    </StyledSection>
   );
 }
+
+const StyledSection = styled.section`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2rem;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    rgba(226, 230, 242, 0) 22.22%,
+    #e2e5f2 93.58%
+  );
+`;
