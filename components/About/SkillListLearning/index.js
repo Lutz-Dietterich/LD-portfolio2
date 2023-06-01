5712419377fe80b046cbd9e10908217d446eada1
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-06-01 13:04:10 +0200

    skill lists compontents

---
+++ components/About/SkillListLearning/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SkillListLearning() {
+  return <div>SkillListLearning</div>;
+}
--- components/About/Skills/index.js
+++ components/About/Skills/index.js
@@ -1,5 +1,36 @@
-import React from "react";
+import styled from "styled-components";
+
+import SkillListNow from "../SkillListNow";
+import SkillListLearning from "../SkillListLearning";
+import SkillListOther from "../SkillListOther";
 
 export default function Skills() {
-  return <div>Skills</div>;
+  return (
+    <StyledSkills>
+      <h2>Skills</h2>
+      <h3>Using Now</h3>
+      <SkillListNow />
+      <h3>Lerning</h3>
+      <SkillListLearning />
+      <h3>Other Skills</h3>
+      <SkillListOther />
+    </StyledSkills>
+  );
 }
+
+const StyledSkills = styled.article`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+
+  h2 {
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+    text-align: center;
+  }
+
+  h3 {
+    margin-bottom: 0.5rem;
+    text-alingn: left;
+    padding-left: 1rem;
+`;
