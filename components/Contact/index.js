f3e080de38447a42a71984c4d42c71eea9565b6c
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-07-27 17:45:43 +0200

    add email Template

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -10,7 +10,7 @@ export default function ContactForm() {
         <h2>Kontakt</h2>
 
         <NodemailerForm />
-        {/* <EmailTemplate /> */}
+        <EmailTemplate />
 
         <StyledAuthor>
           Foto von
