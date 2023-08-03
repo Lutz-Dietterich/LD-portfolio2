7cc5f58fabd1a95eb27cd53a69b6aae0f2b7d17d
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-08-03 20:08:20 +0200

    improve sending email

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -5,7 +5,7 @@ import EmailTemplate from "../../components/EmailTemplate";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { firstName, email, message } = req.body;
+    const { companyName, firstName, lastName, email, message } = req.body;
 
     // Erstelle den Nodemailer Transporter für Strato
     let transporter = nodemailer.createTransport({
@@ -23,9 +23,37 @@ export default async function handler(req, res) {
     );
 
     // Konfiguriere die E-Mail-Daten
+    let subject;
+    let html;
+
+    if (companyName) {
+      subject = "Du hast eine Kontaktanfrage von " + companyName + " erhalten";
+      html = `
+        <h1>Hallo Lutz</h1>
+        <p>Die Firma ${companyName} hat dir folgende Nachricht geschickt:</p>
+        <p>Ansprechpartner: ${firstName} ${lastName}</p>
+        <p>E-Mail: ${email}</p>
+        <p>Nachricht: ${message}</p>
+        <p>Bitte antworte so schnell wie möglich.</p>
+        <p>Dein Kontaktformular</p>
+        <p>PS: Diese E-Mail wurde automatisch generiert. Bitte antworte nicht auf diese E-Mail.</p>
+      `;
+    } else {
+      subject = "Neue Kontaktanfrage von " + firstName + " " + lastName;
+      html = `
+        <h1>Hallo Lutz</h1>
+        <p>Du hast eine neue Kontaktanfrage von ${firstName} ${lastName} erhalten.</p>
+        <p>E-Mail: ${email}</p>
+        <p>Nachricht: ${message}</p>
+        <p>Bitte antworte so schnell wie möglich.</p>
+        <p>Dein Kontaktformular</p>
+        <p>PS: Diese E-Mail wurde automatisch generiert. Bitte antworte nicht auf diese E-Mail.</p>
+      `;
+    }
+
     let mailOptions1 = {
       from: "job@lutz-dietterich.de",
-      to: email, // Empfänger-E-Mail-Adresse eintragen
+      to: email,
       subject: "Neue Nachricht von Lutz Dietterich",
       text: message,
       html: emailContent,
@@ -33,15 +61,15 @@ export default async function handler(req, res) {
 
     let mailOptions2 = {
       from: "job@lutz-dietterich.de",
-      to: "lutzdietterich@googlemail.com", // Zweite Empfänger-E-Mail-Adresse
-      subject: "Du hast eine Kontaktanfrage von" + firstName + " erhalten",
+      to: "lutzdietterich@googlemail.com",
+      subject: subject,
       text: message,
+      html: html,
     };
 
     try {
       // Sende die E-Mail
       await transporter.sendMail(mailOptions1);
-
       await transporter.sendMail(mailOptions2);
 
       res.status(200).json({ message: "E-Mail gesendet" });
