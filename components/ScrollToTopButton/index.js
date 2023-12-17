d5511b79d65cf8abc5a98ad32a24ed74f967ba47
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-12-18 00:30:56 +0100

    size scrollToTop Button

--- components/ScrollToTopButton/index.js
+++ components/ScrollToTopButton/index.js
@@ -38,6 +38,8 @@ export default function ScrollToTopButton() {
 }
 
 const StyledButton = styled.button`
+  max-width: 2rem;
+  max-height: 2rem;
   position: fixed;
   bottom: 70px;
   right: 20px;
