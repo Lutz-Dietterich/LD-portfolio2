88709f4e6ffee6596c120867031c950356dff6b6
Lutz Dietterich <lutzdietterich@googlemail.com>
2025-08-26 13:14:10 +0200

    create SkillTag Component

---
+++ components/cv/SkillTag/index.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function SkillTag({ text }) {
+    return (
+        <div>
+            <StyledSkillTag>{text}</StyledSkillTag>
+        </div>
+    );
+}
+
+const StyledSkillTag = styled.dev`
+    background: rgba(255, 255, 255, 0.2);
+    padding: 3px 6px;
+    border-radius: 10px;
+    font-size: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.3);
+`;
